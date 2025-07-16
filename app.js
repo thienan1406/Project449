04d7515d63126ab3e5180b31997339ccce0e2d5a
Thien An <nguyenthienan327@gmail.com>
2025-07-16 21:12:55 +0700

    Project 449

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const cors = require("cors");
 
 const app = express();
 
+const contactsRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/contacts", contactsRouter);
+
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to book" });
+  res.json({ message: "Welcome to book application" });
 });
 
 module.exports = app;
